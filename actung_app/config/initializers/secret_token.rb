baca1726445669fc7f530ee03f63463a1f0e1432
Maximilian Holden Perchik <maximilianhp@me.com>
2014-03-18 15:41:44 +0000

    initial commit

---
+++ actung_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ActungApp::Application.config.secret_token = '9e9ddfb1fc0db3e879680099ca99da1b02e6f798a34f39db3279a8b76d71ba32b82a29e9d24bb97a03e38afabae6a90910ad0e1927bef2b730fd178d14376481'
